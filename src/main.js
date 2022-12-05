6997765d754eb3f1dbac2bfb1da01e54dfb3ea7a
Felix Lahnsteiner <felix.lahnsteiner@students.fhv.at>
2022-12-05 21:47:05 +0100

    passwords.json, stored at $DESKTOP, can be shown

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 const { invoke } = window.__TAURI__.tauri;
 const { readTextFile, BaseDirectory } = window.__TAURI__.fs;
+const { desktopDir } = window.__TAURI__.path;
 
 let test_button;
 let echo_paragraph;
 let text_input;
+
+function fileReadError(message, error) {
+  console.error(error);
+  showMessage(message);
+}
 
 function showMessage(response) {
   echo_paragraph.textContent = response;
@@ -11,8 +17,13 @@ function showMessage(response) {
 
 async function echo() {
   console.log("button pressed");
-  const contents = await readTextFile('C:/Users/Felix Lahnsteiner/git/desktopWebapplikationen/PasswordManager/src/passwords.json', { contents: String });
-  console.log(contents);
+  let path = await desktopDir();
+  let filename = "passwords.json";
+  let filepath = path + filename;
+    
+    const contents = await readTextFile(filepath, { path: path, contents: String })
+    .then(file => { showMessage(file) })
+    .catch(error => { fileReadError("cannont read file", error) });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
