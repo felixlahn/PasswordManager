c861131bd149445a317a784e6a2585986c67376a
Felix Lahnsteiner <felix.lahnsteiner@students.fhv.at>
2022-12-06 17:06:44 +0100

    refactor system menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,7 @@
   windows_subsystem = "windows"
 )]
 
-use tauri::{Manager, Window, Submenu, CustomMenuItem, Menu};
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu, Window};
 
 #[derive(Debug, serde::Serialize)]
 enum EncryptionError {
@@ -26,17 +26,26 @@ fn decrypt(ciphertext: String, password: String, window: Window) -> Result<Strin
 }
 
 fn main() {
+  let quit = CustomMenuItem::new("open".to_string(), "Open").accelerator("cmdOrControl+O");
+  let close = CustomMenuItem::new("close".to_string(), "Close").accelerator("cmdOrControl+Q");
+  let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+  let menu = Menu::new()
+    .add_submenu(submenu);
+
   tauri::Builder::default()
+      .menu(menu)
+      .on_menu_event(|event| {
+        match event.menu_item_id() {
+          "close" => {
+            event.window().close().unwrap();
+          }
+          "open" => {
+            println!("open");
+          }
+          _ => {}
+        }
+      })
       .invoke_handler(tauri::generate_handler![decrypt])
-      .menu(Menu::new().add_submenu(Submenu::new("File",
-        Menu::new()
-          .add_item(CustomMenuItem::new("close", "Close").accelerator("cmdOrControl+Q")))))
-          .on_menu_event(|event| match event.menu_item_id() {
-            "close" => {
-                event.window().close().unwrap();
-            }
-            _ => {}
-        })
       .run(tauri::generate_context!())
       .expect("error while running tauri application");
 }
