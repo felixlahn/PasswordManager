567fba81dad50bd6f614655e8c4fbf6109fb9172
Felix Lahnsteiner <felix.lahnsteiner@students.fhv.at>
2022-12-22 10:21:09 +0100

    entries from encrypted passwordfile are shown in table

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const { listen } = window.__TAURI__.event;
 const { open } = window.__TAURI__.dialog;
 const { readTextFile } = window.__TAURI__.fs;
 
-import { output_paragraph, second_out, first_button } from './modules/htmlElements.mjs'
+import { first_button, password_table } from './modules/htmlElements.mjs'
 
 let decryptedDeserializedPasswords;
 let password = "myFancyPassword";
@@ -14,6 +14,71 @@ async function handleMenueEvent(eventPayloadMessage) {
   }
 }
 
+function showPasswords(passwords) {
+  //clear all rows that may exist
+  let tableHeaderRowCount = 1;
+  let table = password_table;
+  let rowCount = table.rows.length;
+  for (var i = tableHeaderRowCount; i < rowCount; i++) {
+      table.deleteRow(tableHeaderRowCount);
+  }
+  
+  table = password_table.getElementsByTagName('tbody')[0];
+
+  passwords.entries.forEach(element => {
+    var newRow = table.insertRow();
+    // accountname
+    var accountNameCell = newRow.insertCell();
+    accountNameCell.classList.add("lalign");
+    var accountNameCellText = document.createTextNode(element.name);
+    accountNameCell.appendChild(accountNameCellText);
+
+    // username
+    var usernameCell = newRow.insertCell();
+    var usernameCellText = document.createTextNode(element.username);
+    usernameCell.appendChild(usernameCellText);
+
+    //password
+    var passwordCell = newRow.insertCell();
+    var passwordCellText = document.createTextNode(element.password);
+    passwordCell.appendChild(passwordCellText);
+
+    // url
+    var urlCell = newRow.insertCell();
+    var urlCellText = document.createTextNode(element.url);
+    urlCell.appendChild(urlCellText);
+
+    // buttons
+    var emptyCell = newRow.insertCell()
+    var buttonCell = newRow.insertCell();
+    newRow.appendChild(emptyCell);
+
+    var editButton = document.createElement("a");
+    editButton.setAttribute("href", "#");
+    editButton.className = "button edit";
+    buttonCell.appendChild(editButton);
+
+    var deleteButton = document.createElement("a");
+    deleteButton.setAttribute("href", "#");
+    deleteButton.className = "button delete";
+    buttonCell.appendChild(deleteButton);
+
+    var confirmButton = document.createElement("a");
+    confirmButton.setAttribute("href", "#");
+    confirmButton.setAttribute("style", "display:none");
+    confirmButton.className = "button confirm";
+    buttonCell.appendChild(confirmButton);
+    
+    var cancelButton = document.createElement("a");
+    cancelButton.setAttribute("href", "#");
+    cancelButton.setAttribute("style", "display:none");
+    cancelButton.className = "button cancel";
+    buttonCell.appendChild(cancelButton);
+
+    newRow.appendChild(buttonCell);
+  });
+}
+
 async function openAndDecrypt(password) {
   let filePath = await open();
   let contents = await readTextFile(filePath);
@@ -21,11 +86,12 @@ async function openAndDecrypt(password) {
   await invoke("decrypt", { cyphertext: contents, password: password })
       .then((plaintext) => {
         decryptedDeserializedPasswords = JSON.parse(plaintext);
-        console.log("deserialized", decryptedDeserializedPasswords);
       })
       .catch((err) => {
         console.error(err);
       });
+
+  showPasswords(decryptedDeserializedPasswords);
 }
 
 const unlistenMenuEvent = listen(
--- src/modules/htmlElements.mjs
+++ src/modules/htmlElements.mjs
@@ -1,13 +1,16 @@
 export let output_paragraph;
 export let second_out;
 export let first_button;
+export let password_table;
 
 let output_paragraph_id = "output-paragraph";
 let second_output_paragraph_id = "second-output";
 let first_button_id = "btn_enc_dec";
+let password_table_id = "pw-entries"
 
 window.addEventListener("DOMContentLoaded", () => {
     first_button = document.getElementById(first_button_id);
     output_paragraph = document.getElementById(output_paragraph_id);
     second_out = document.getElementById(second_output_paragraph_id);
+    password_table = document.getElementById(password_table_id);
 })
